3ea56f0e416d448494697efd20f1d8a0d56147ef
Jaime Paredes <jjpp109309@gmail.com>
2022-06-01 05:48:47 +0000

    general and colorschemes in native lua

---
+++ lua/general.lua
@@ -0,0 +1,32 @@
+--language en_US
+
+--split navigation
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+
+--layout
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.ts = 4
+vim.opt.sw = 4
+vim.opt.showmatch = true
+vim.opt.scrolloff = 2
+vim.opt.wrap = false
+vim.opt.colorcolumn = '80'
+
+--insert mode remaps 
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('i', '(', '()<Left>')
+vim.keymap.set('i', '[', '[]<Left>')
+vim.keymap.set('i', '{', '{}<Left>')
+vim.keymap.set('i', '\'', '\'\'<Left>')
+vim.keymap.set('i', '\"', '\"\"<Left>')
+
+--editor
+vim.opt.cursorline = true
+
+--set termguicolors
+vim.opt.background = 'dark'
+
