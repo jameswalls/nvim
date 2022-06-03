1d210057993bedc679fface1baf8d71d1cce6f5d
Jaime Paredes <jjpp109309@gmail.com>
2022-06-02 19:32:41 -0500

    enable treesitter

--- init.lua
+++ init.lua
@@ -1,11 +1,9 @@
 vim.cmd('source $HOME/.config/nvim/modules/plugins.vim')
 require('general')
 require('lsp')
+require('treesitter')
+require('colorschemes.gruvbox')
 
---vim.cmd('source $HOME/.config/nvim/modules/treesitter.vim')
 vim.cmd('source $HOME/.config/nvim/modules/telescope.vim')
 --vim.cmd('source $HOME/.config/nvim/modules/python.vim')
 --vim.cmd('source $HOME/.config/nvim/modules/gitsigns.vim')
-
-require('colorschemes.gruvbox')
-
