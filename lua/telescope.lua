bfc2769ac71a06a416c3d1bf083f2998a6c0a527
Jaime Paredes <jjpp109309@gmail.com>
2022-06-02 20:30:18 -0500

    add lua telescope remaps

---
+++ lua/telescope.lua
@@ -0,0 +1,7 @@
+-- Find files using Telescope command-line sugar.
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope grep_string<cr>')
