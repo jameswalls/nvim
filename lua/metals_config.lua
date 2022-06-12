91d67d14c55768f70723280f13152f559d864462
Jaime Paredes <jjpp109309@gmail.com>
2022-06-11 21:53:01 -0500

    metals status bar config

--- lua/metals_config.lua
+++ lua/metals_config.lua
@@ -1,7 +1,10 @@
 -- scala: metals
 vim.opt_global.shortmess:remove("F")
-local metals_config = require("metals").bare_config()
-local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
+metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
+
+nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "scala", "sbt", "java" },
 	callback = function()
