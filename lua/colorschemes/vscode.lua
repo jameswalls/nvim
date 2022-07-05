b59d9b784f1aca35e4c7bc7d4451ff1c94f24975
Jaime Paredes <jjpp109309@gmail.com>
2022-07-05 12:49:49 -0500

    Update vscode.lua

--- lua/colorschemes/vscode.lua
+++ lua/colorschemes/vscode.lua
@@ -1,16 +1,3 @@
-" Vim-Script:
-" For dark theme
-let g:vscode_style = "dark"
-
-" For light theme
-" let g:vscode_style = "light"
-" Enable transparent background.
-" let g:vscode_transparency = 1
-
-" Enable italic comment
-" let g:vscode_italic_comment = 1
-" Disable nvim-tree background color 
-" let g:vscode_disable_nvimtree_bg = v:true 
-
-colorscheme vscode
-
+vim.o.background = "dark"
+vim.g.vscode_transparent = 1
+vim.cmd([[colorscheme vscode]])
