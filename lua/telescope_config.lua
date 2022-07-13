eb98b913e13241e0f7a3c707de01f0181cacd762
Jose Paredes <jose.paredes@nubank.com.br>
2022-07-13 11:56:00 -0500

    remap telescope comands

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -11,13 +11,13 @@ require('telescope').load_extension('file_browser')
 
 -- Find files using Telescope command-line sugar.
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>lg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<cr>')
 vim.keymap.set('n', '<leader>gs', '<cmd>Telescope grep_string<cr>')
-vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>')
-vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>')
+vim.keymap.set('n', '<leader>df', '<cmd>Telescope lsp_definitions<cr>')
+vim.keymap.set('n', '<leader>rf', '<cmd>Telescope lsp_references<cr>')
 
 
 -- filebrowser
