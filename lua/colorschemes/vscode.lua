392371437a9218130bfab146d4cdc715f19852ad
Jose Paredes <jose.paredes@nubank.com.br>
2022-07-20 00:33:40 -0500

    configure lua vscode

--- lua/colorschemes/vscode.lua
+++ lua/colorschemes/vscode.lua
@@ -1,3 +1,36 @@
-vim.o.background = "dark"
---vim.g.vscode_transparent = 1
---vim.cmd([[colorscheme vscode]])
+-- Lua:
+-- For dark theme (neovim's default)
+vim.o.background = 'dark'
+-- For light theme
+--vim.o.background = 'light'
+
+local c = require('vscode.colors')
+require('vscode').setup({
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        --vscLineNumber = '#FFFFFF',
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        --Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+		TSConstructor = { fg = c.vscBlueGreen, bg = 'NONE' },
+		TSType = { fg = c.vscBlueGreen, bg = 'NONE' },
+		TSFuncBuiltin = { fg = c.vscMediumBlue, bg = 'NONE' },
+		TSKeywordOperator = { fg = c.vscPink, bg = 'NONE' },
+		TSMethod = { fg = c.vscFront, bg = 'NONE' },
+		TSKeyword = { fg = c.vscBlue, bg = 'NONE' },
+		TSKeywordReturn = { fg = c.vscPink, bg = 'NONE' },
+    }
+})
