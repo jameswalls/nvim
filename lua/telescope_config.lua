821b00fee89cb18ce8b9c5ce110b8c9905aa65d1
Jose Paredes <jose.paredes@nubank.com.br>
2022-08-18 14:26:30 -0500

    edit telescope commands

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -7,21 +7,18 @@ require('telescope').setup{
 
 -- additional extensions
 require('telescope').load_extension('fzf')
-require('telescope').load_extension('file_browser')
 
 -- Find files using Telescope command-line sugar.
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>lg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<cr>')
 vim.keymap.set('n', '<leader>gs', '<cmd>Telescope grep_string<cr>')
-vim.keymap.set('n', '<leader>df', '<cmd>Telescope lsp_definitions<cr>')
+vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>')
 vim.keymap.set('n', '<leader>td', '<cmd>Telescope lsp_type_definitions<cr>')
-vim.keymap.set('n', '<leader>rf', '<cmd>Telescope lsp_references<cr>')
-vim.keymap.set('n', '<leader>im', '<cmd>Telescope lsp_implementation<cr>')
+vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>')
+vim.keymap.set('n', '<leader>gi', '<cmd>Telescope lsp_implementation<cr>')
 vim.keymap.set('n', '<leader>dg', '<cmd>Telescope diagnostics<cr>')
 
 
--- filebrowser
-vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h <cr>", { noremap = true })
