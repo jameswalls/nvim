3f6a8362d927d3437b273ce6607404cf31987b4c
J. Jaime Paredes <jjpp109309@gmail.com>
2023-07-24 10:02:02 -0600

    remap git_files telescope command

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -27,11 +27,11 @@ require('telescope').setup{
 require('telescope').load_extension('fzf')
 
 -- Find files using Telescope command-line sugar.
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope git_files<cr>')
+vim.keymap.set('n', '<leader>fa', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-vim.keymap.set('n', '<leader>gf', '<cmd>Telescope git_files<cr>')
 vim.keymap.set('n', '<leader>gs', '<cmd>Telescope grep_string<cr>')
 vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>')
 vim.keymap.set('n', '<leader>td', '<cmd>Telescope lsp_type_definitions<cr>')
