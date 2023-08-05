bd4ccf5525da0f4795afdc75dd2280ae6d1a6d23
J. Jaime Paredes <jjpp109309@gmail.com>
2023-08-04 23:59:39 -0600

    nvim tree theme

--- lua/colorschemes/vscode-config.lua
+++ lua/colorschemes/vscode-config.lua
@@ -2,7 +2,7 @@
 -- For dark theme (neovim's default)
 vim.o.background = 'dark'
 -- For light theme
---vim.o.background = 'light'
+-- vim.o.background = 'light'
 
 local c = require('vscode.colors').get_colors()
 
@@ -14,7 +14,7 @@ require('vscode').setup({
     italic_comments = false,
 
     -- Disable nvim-tree background color
-    disable_nvimtree_bg = false,
+    disable_nvimtree_bg = true,
 
     -- Override colors (see ./lua/vscode/colors.lua)
     color_overrides = {
@@ -23,9 +23,8 @@ require('vscode').setup({
 
     -- Override highlight groups (see ./lua/vscode/theme.lua)
     group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        --Variable = { fg=c.vscYellow, bg=c.vscLightGreen, bold=true },
+		-- this supports the same val table as vim.api.nvim_set_hl use colors from this colorscheme by requiring vscode.colors!
+        -- Variable = { fg=c.vscYellow, bg=c.vscLightGreen, bold=true },
 		['@constructor'] = { fg=c.vscOrange },
 		['@type.builtin'] = { fg=c.vscBlueGreen },
 		['@function.builtin'] = { fg=c.vscBlueGreen },
