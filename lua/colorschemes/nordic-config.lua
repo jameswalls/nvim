cb790221d6c7b2bd07e92e7c205d85f7d3f591d0
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-27 17:32:11 -0600

    configure nord

--- lua/colorschemes/nordic-config.lua
+++ lua/colorschemes/nordic-config.lua
@@ -6,11 +6,11 @@ require 'nordic' .setup {
     -- Enable italic comments.
     italic_comments = true,
     -- Enable general editor background transparency.
-    transparent_bg = false,
+    transparent_bg = true,
     -- Enable brighter float border.
     bright_border = false,
     -- Reduce the overall amount of blue in the theme (diverges from base Nord).
-    reduced_blue = true,
+    reduced_blue = false,
     -- Swap the dark background with the normal one.
     swap_backgrounds = true,
     -- Override the styling of any highlight group.
