cbdc56e23982e4fb28fad13645bff80e1ef4f2a9
Jaime Paredes <jjpp109309@gmail.com>
2023-12-21 12:10:17 -0600

    Update harpoon-config.lua

--- lua/harpoon-config.lua
+++ lua/harpoon-config.lua
@@ -1,13 +1,10 @@
-local harpoon = require("harpoon")
+vim.keymap.set("n", "<space>m", "<cmd>lua require(\"harpoon.mark\").add_file()<cr>")
+vim.keymap.set("n", "<space>h", "<cmd>lua require(\"harpoon.ui\").toggle_quick_menu()<cr>")
 
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+vim.keymap.set("n", "<space>n", "<cmd>lua require(\"harpoon.ui\").nav_next()<cr>")
+vim.keymap.set("n", "<space>p", "<cmd>lua require(\"harpoon.ui\").nav_prev()<cr>")
 
-vim.keymap.set("n", "<space>m", function() harpoon:list():append() end)
-vim.keymap.set("n", "<space>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<space>q", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<space>w", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<space>e", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<space>r", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<space>q", "<cmd>lua require(\"harpoon.ui\").nav_file(1)<cr>")
+vim.keymap.set("n", "<space>w", "<cmd>lua require(\"harpoon.ui\").nav_file(2)<cr>")
+vim.keymap.set("n", "<space>e", "<cmd>lua require(\"harpoon.ui\").nav_file(3)<cr>")
+vim.keymap.set("n", "<space>r", "<cmd>lua require(\"harpoon.ui\").nav_file(4)<cr>")
