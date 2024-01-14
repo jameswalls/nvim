30622be58a8ce332502db5fa5a545a32bfb00932
J. Jaime Paredes <jjpp109309@gmail.com>
2024-01-13 22:03:15 -0600

    configure rose pine

--- lua/colorschemes/rose_pine.lua
+++ lua/colorschemes/rose_pine.lua
@@ -1,69 +1,78 @@
 require('rose-pine').setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'auto',
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = true,
-	disable_float_background = true,
-	disable_italics = true,
+    variant = "auto", -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
 
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		background_nc = '_experimental_nc',
-		panel = 'surface',
-		panel_nc = 'base',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
+    styles = {
+        bold = false,
+        italic = false,
+        transparency = true,
+    },
 
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
 
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		-- ColorColumn = { bg = 'rose' },
-		-- -- Blend colours against the "base" background
-		-- CursorLine = { bg = 'foam', blend = 10 },
-		-- StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-		--
-		-- -- By default each group adds to the existing config.
-		-- -- If you only want to set what is written in this config exactly,
-		-- -- you can set the inherit option:
-		-- Search = { bg = 'gold', inherit = false },
-		['@boolean'] = { fg='love', inherit=false },
-		['@field'] = { fg='iris', inherit=false },
-		['@function.builtin'] = { fg='rose', inherit=false },
-		['@function.call'] = { fg='rose', inherit=false },
-		['@function.macro'] = { fg='rose', inherit=false },
-		['@namespace'] = { fg='text', inherit=false },
-		['@method.call'] = { fg='iris', inherit=false },
-		['@number'] = { fg='love', inherit=false },
-		['@parameter'] = { fg='foam', inherit=false },
-		['@string'] = { fg='foam', inherit=false },
-		['@type'] = { fg='text', inherit=false },
-		['@type.builtin'] = { fg='iris', inherit=false },
-		['@type.qualifier'] = { fg='iris', inherit=false },
-		['@variable.builtin'] = { fg='foam', inherit=false },
-	}
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        headings = {
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+        },
+        -- Alternatively, set all headings at once.
+        -- headings = "subtle",
+    },
+
+    highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+		-- ['@boolean'] = { fg='love', inherit=false },
+		-- ['@field'] = { fg='iris', inherit=false },
+		-- ['@function.builtin'] = { fg='rose', inherit=false },
+		-- ['@function.call'] = { fg='rose', inherit=false },
+		-- ['@function.macro'] = { fg='rose', inherit=false },
+		-- ['@namespace'] = { fg='text', inherit=false },
+		-- ['@method.call'] = { fg='iris', inherit=false },
+		-- ['@number'] = { fg='love', inherit=false },
+		-- ['@parameter'] = { fg='foam', inherit=false },
+		-- ['@string'] = { fg='foam', inherit=false },
+		-- ['@type'] = { fg='text', inherit=false },
+		-- ['@type.builtin'] = { fg='iris', inherit=false },
+		-- ['@type.qualifier'] = { fg='iris', inherit=false },
+		-- ['@variable.builtin'] = { fg='foam', inherit=false },
+    },
+
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
 })
 
-vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme("rose-pine")
