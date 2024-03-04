b258c826a4746498c9b9b1b131e5102eb9d1323f
J. Jaime Paredes <jjpp109309@gmail.com>
2024-03-03 18:55:24 -0600

    Pimp telescope config

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,7 +1,19 @@
 -- initial telescope
+local builtin = require("Telescope.builtin")
+local extensions = require("Telescope").extensions
+
 require('telescope').setup({
 	defaults = {
 		wrap_results = true,
+	},
+	pickers = {
+		colorscheme = {
+			enable_preview = true
+		},
+		current_buffer_fuzzy_find = {
+			theme = "dropdown",
+			previewer = false
+		}
 	},
 	extensions = {
 		fzf = {
@@ -13,6 +25,7 @@ require('telescope').setup({
 		live_grep_args = {
 			auto_quoting = true, -- enable/disable auto-quoting
 			theme = "dropdown",
+			previewer = false
 		}
 	}
 })
@@ -21,16 +34,16 @@ require('telescope').setup({
 require("telescope").load_extension("fzf")
 
 -- Find files using Telescope command-line sugar.
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files theme=dropdown<cr>')
-vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>")
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>gs', '<cmd>Telescope grep_string theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>td', '<cmd>Telescope lsp_type_definitions theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>gi', '<cmd>Telescope lsp_implementation theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>dg', '<cmd>Telescope diagnostics theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>cc', '<cmd>Telescope commands theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>bb', '<cmd>Telescope buffers theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>ss', '<cmd>Telescope current_buffer_fuzzy_find theme=dropdown<cr>')
-vim.keymap.set('n', '<leader>cs', '<cmd>Telescope colorscheme theme=dropdown<cr>')
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+vim.keymap.set('n', '<leader>gs', builtin.grep_string)
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions)
+vim.keymap.set('n', '<leader>td', builtin.lsp_type_definitions)
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references)
+vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations)
+vim.keymap.set('n', '<leader>dg', builtin.diagnostics)
+vim.keymap.set('n', '<leader>cc', builtin.commands)
+vim.keymap.set('n', '<leader>bb', builtin.buffers)
+vim.keymap.set('n', '<leader>cs', builtin.colorscheme)
+vim.keymap.set('n', '<leader>ss', builtin.current_buffer_fuzzy_find)
+vim.keymap.set("n", "<leader>fg", extensions.live_grep_args.live_grep_args)
