832a5e31ebaeeb33b40529007339315194c20567
Jaime Paredes <jaime.paredes@tala.co>
2024-03-13 12:57:53 -0600

    git status

--- lua/colorschemes/kanagawa.lua
+++ lua/colorschemes/kanagawa.lua
@@ -1,13 +1,13 @@
 -- Default options:
 require('kanagawa').setup({
     compile = false,             -- enable compiling the colorscheme
-    undercurl = true,            -- enable undercurls
-    commentStyle = { italic = false },
-    functionStyle = {},
-    keywordStyle = { italic = false},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = true,         -- do not set background color
+    undercurl = false,            -- enable undercurls
+    commentStyle = { italic = false, bold = false },
+    functionStyle = { bold = false },
+    keywordStyle = { italic = false, bold = false},
+    statementStyle = { bold = false },
+    typeStyle = { bold = false },
+    transparent = false,         -- do not set background color
     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
     colors = {                   -- add/modify theme and palette colors
@@ -17,9 +17,8 @@ require('kanagawa').setup({
     overrides = function(colors) -- add/modify highlights
         return {}
     end,
-    -- theme = "wave",              -- Load "wave" theme when 'background' option is not set
     background = {               -- map the value of 'background' option to a theme
-        dark = "wave",           -- try "dragon" !
+        dark = "dragon",           -- try "dragon" !
         light = "lotus"
     },
 })
