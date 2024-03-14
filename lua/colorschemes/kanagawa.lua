9b807c6b85f752c2ed506407edb727e500a4f879
J. Jaime Paredes <jjpp109309@gmail.com>
2024-03-13 19:41:09 -0600

    Cofnigure kanawa border colors

--- lua/colorschemes/kanagawa.lua
+++ lua/colorschemes/kanagawa.lua
@@ -7,7 +7,7 @@ require('kanagawa').setup({
     keywordStyle = { italic = false, bold = false},
     statementStyle = { bold = false },
     typeStyle = { bold = false },
-    transparent = false,         -- do not set background color
+    transparent = true,         -- do not set background color
     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
     colors = {                   -- add/modify theme and palette colors
@@ -19,11 +19,13 @@ require('kanagawa').setup({
 			Keyword = { fg = colors.theme.syn.keyword, bold = false },
 			["@keyword.operator"] = { fg = colors.theme.syn.operator, bold = false },
 			Boolean = { fg = colors.theme.syn.constant, bold = false },
-			LineNr = { fg = colors.theme.ui.nontext, bg = colors.theme.ui.bg },
-			SignColumn = { fg = colors.theme.ui.special, bg = colors.theme.ui.bg },
-			GitSignsAdd = { fg = colors.theme.vcs.added, bg = colors.theme.ui.bg },
-			GitSignsChange = { fg = colors.theme.vcs.changed, bg = colors.theme.ui.bg },
-			GitSignsDelete = { fg = colors.theme.vcs.removed, bg = colors.theme.ui.bg },
+			-- LineNr = { fg = colors.theme.ui.nontext, bg = colors.theme.ui.bg },
+			LineNr = { fg = colors.theme.ui.nontext, bg = "None" },
+			SignColumn = { fg = colors.theme.ui.special, bg = "None" },
+			GitSignsAdd = { fg = colors.theme.vcs.added, bg = "None" },
+			GitSignsChange = { fg = colors.theme.vcs.changed, bg = "None" },
+			GitSignsDelete = { fg = colors.theme.vcs.removed, bg = "None" },
+			TelescopeBorder = { bg = "None"}
 		}
     end,
     background = {               -- map the value of 'background' option to a theme
