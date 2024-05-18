a7032dc7c9f4f26d2a8244d6ad8d92ca767e6144
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-18 01:38:06 -0600

    Minimal lazy config

--- init.lua
+++ init.lua
@@ -1,20 +1,45 @@
-require("plugins")
-require("general")
-require("globals")
-require("luasnip-config")
-require("cmp-config")
-require("web-dev-icons-config")
-require("neodev-config")
-require("telescope-config")
-require("lsp-config")
-require("harpoon-config")
-require("gitsigns-config")
-require("lualine-config")
-require('autopairs-config')
-require("treesitter-config")
-require("nvim-tree-config")
-require("context-config")
-require("cloak-config")
-require("dap.dap-python")
-require("fidget-config")
-require("colorschemes.one-dark")
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+
+vim.opt.ts = 4
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
+vim.opt.scrolloff = 10
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
