342943b680310ec1ccb8fb3c17c67ce409fe80c6
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-18 12:53:12 -0600

    Dont load colorscheme

--- init.lua
+++ init.lua
@@ -50,4 +50,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	install = {
+		missing = true,
+		colorscheme = {}
+	}
+})
