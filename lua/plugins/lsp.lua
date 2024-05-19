c773ba57dabc1ab05a6d8b46b43b0982ed7ac403
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-19 02:59:58 -0600

    remove lsp highlights

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,30 +36,6 @@ return {
 				map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 				map('K', vim.lsp.buf.hover, 'Hover Documentation')
 				map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.server_capabilities.documentHighlightProvider then
-					local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
-
-					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
-
-					vim.api.nvim_create_autocmd('LspDetach', {
-						group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-						end,
-					})
-				end
 			end,
 		})
 
