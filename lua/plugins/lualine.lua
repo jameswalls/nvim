fa55bb3cf7aeb577e24b75ac968d1f5d943aced6
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-19 21:05:43 -0600

    Show parent dir and filename

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -31,7 +31,7 @@ return {
 			sections = {
 				lualine_a = {"mode"},
 				lualine_b = {"branch", "diff", "diagnostics"},
-				lualine_c = {"filename"},
+				lualine_c = {{"filename", path=4}},
 				lualine_x = {"filetype"},
 				lualine_y = {"progress"},
 				lualine_z = {"location"}
