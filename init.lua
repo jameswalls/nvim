179d235c44392972d0e6b88cafce64c185fe3613
J. Jaime Paredes <jjpp109309@gmail.com>
2024-05-19 18:03:24 -0600

    Adjust select on yank autocmd

--- init.lua
+++ init.lua
@@ -35,9 +35,9 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank({higroup="Visual"})
   end,
 })
 
